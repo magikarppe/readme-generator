3ec55b076fc8e7d8ccca7f7f8fd3b33c875699a4
Thalia De Mattos <tgdmattos@gmail.com>
2021-05-10 13:05:50 -0400

    changed files and optimized generator

--- index.js
+++ index.js
@@ -1,80 +1,78 @@
 // TODO: Include packages needed for this application
-const inquirer = require("inquirer");
-const fs = require("fs");
-const markdown = require("./utils/generateMarkdown");
-// const { log } = require("console");
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const markdown = require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'titleName',
-        message: 'What is the title of your project?',
-    },
-    {
-        type: 'input',
-        name: 'userName',
-        message: 'What is your Github username?',
-        default: 'magikarppe',
-    },
-    {
-        type: 'input',
-        name: 'emailName',
-        message: 'What is your email?',
-        default: 'tgdmattos@gmail.com'
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'What license would you like to use?',
-        choices: ['MIT', 'GPLv2', 'Apache', 'GPLv3', 'BSD 3-clause', 'none'],
-    },
-    {
-        type: 'input',
-        name: 'install',
-        message: 'What commands to install dependencies?',
-        default: ['npm i'],
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Write a short description about project.',
-    },
-    {
-        type: 'input',
-        name: 'runTest',
-        message: 'What commands to run tests?',
-        default: ['npm test'],
-    },
-    {
-        type: 'input',
-        name: 'userClone',
-        message: 'What user needs to know for using repo?',
-        default: ['git clone'],
-    },
-    {
-        type: 'input',
-        name: 'contribute',
-        message: 'What user needs to know to contribute to the repo?',
-        default: ['Create pull request'],
-    },
+const questions = [   
+{type: 'input',
+  message: 'What is your GitHub username?',
+  name: 'username',
+},
+{
+  type: 'input',
+  message: 'What is your email address?',
+  name: 'email',
+},
+{
+  type: 'input',
+  message: 'What is the name of your project?',
+  name: 'name',
+},
+{
+  type: 'input',
+  message: 'Write a short description of your project:',
+  name: 'description',
+},
+{
+  type: 'input',
+  message: 'Please enter the link to your deployed project:',
+  name: 'link',
+},
+{
+  type: 'list',
+  message: 'What kind of license should your project have',
+  choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'IBM 1.0', 'None'],
+  name: 'license',
+},
+{
+  type: 'input',
+  message: 'What command should be run to install dependencies?',
+  default: 'npm i',
+  name: 'install',
+},
+{
+  type: 'input',
+  message: 'What command should be run to run tests?',
+  default: 'npm test',
+  name: 'tests',
+},
+
+{
+  type: 'input',
+  message: 'What does the user need to know about using your repository?',
+  name: 'usage',
+},
+
+{
+  type: 'input',
+  message: 'What does the user need to know about contributing to your repository?',
+  name: 'contribute',
+},
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>
-    err ? console.log(err) : console.log("Success")); 
+  fs.writeFile(fileName, data, (err) =>
+  err ? console.log(err) : console.log ('Success!'));
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then((data) => {
-        console.log(data);
-        let answers = markdown(data);
-        console.log("This is the template" + answers);
-        writeToFile("README.md", answers)
-    })
+  inquirer.prompt(questions).then((data) => {
+ console.log(data);
+ let responses = markdown(data);
+ writeToFile("README.md", responses);
+  })
 }
-
 // Function call to initialize app
 init();
